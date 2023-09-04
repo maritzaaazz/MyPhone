a5c22c50a87029aecb51372f35d052cbb913fb52
maritzaaazz <maritzaangelinaazzahra@mail.ugm.ac.id>
2023-09-04 23:18:50 +0700

    Initial commit

--- app/src/main/java/com/example/myphone/MainActivity.kt
+++ app/src/main/java/com/example/myphone/MainActivity.kt
@@ -8,12 +8,18 @@ import com.example.myphone.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var binding=ActivityMainBinding.inflate(layoutInflater)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        with(binding){
-            login.setOnClickListener{
-                Toast.makeText(this@MainActivity,"Hello Guys!", Toast.LENGTH_LONG).show()
+        val usernameEditText = binding.editTextUsername
+        with(binding) {
+            login.setOnClickListener {
+                val username = usernameEditText.text.toString().trim()
+                if (username.isNotEmpty()) {
+                    val message = "Hello $username!"
+                    Toast.makeText(this@MainActivity, message, Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this@MainActivity, "Please enter a username", Toast.LENGTH_SHORT).show()
+                }
             }
         }
     }
